37fe4903f340a4da5b9a65378e20cac94856ce07
Gary Berkinfand <gberkinfand@gmail.com>
2021-01-16 17:24:53 +0200

    Adding login and logout with authorization tokens

--- backend/django_app/mainapp/settings.py
+++ backend/django_app/mainapp/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'prediction',
+    
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
+
+    'prediction',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
