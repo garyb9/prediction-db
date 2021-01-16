e1863975f0d3d3e2abb324255e40018ee715a3ce
Gary Berkinfand <gberkinfand@gmail.com>
2021-01-16 17:29:43 +0200

    Adding password change

--- backend/django_app/users/urls.py
+++ backend/django_app/users/urls.py
@@ -4,4 +4,5 @@ from users.views import APILoginView, APILogoutView
 urlpatterns = [
     path('login/', APILoginView.as_view(), name='api_login'),
     path('logout/', APILogoutView.as_view(), name='api_logout'),
+    path('update_password/', APIPasswordUpdateView.as_view(), name='api_update_password'),
 ]
